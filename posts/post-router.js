dd42b541a5bd198fbbeec217074d900be252451e
spazdrum <spazdrum@gmail.com>
2020-09-15 23:22:58 -0400

    post('/')

--- posts/post-router.js
+++ posts/post-router.js
@@ -15,18 +15,41 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
-  db("posts")
-    .where("/:id", req.params.id)
-    .then((posts) => {
-      res.json(posts);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "invalid ID", error: err });
-    });
+router.get("/:id", await, (req, res) => {
+//   db("posts")
+//     .where("/:id", req.params.id)
+//     .then((posts) => {
+//       res.json(posts);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ message: "invalid ID", error: err });
+//     });
+    const { id } = req.params;
+
+    try {
+        const [post] = await db('posts').where({ id });
+
+        if (post) {
+            res.json(post);
+        } else {
+            res.status(404).json({ message: 'Bad ID'})
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'db error', error : err })
+    }
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async, (req, res) => {
+    const newPost = req.body;
+    try {
+        const sql = await db('posts').insert(newPost).toString();
+        console.log(sql);
+        const post = db('posts').insert(newPost);
+        res.status(201).json(post);
+    } catch (err) {
+        res.status(500).json({ message: 'error posting', error : err})
+    }
+});
 
 router.put("/:id", (req, res) => {});
 
