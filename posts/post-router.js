8724938fe4004585c68791fda7ce3e90ee2eec53
spazdrum <spazdrum@gmail.com>
2020-09-15 23:38:39 -0400

    post-router completed

--- posts/post-router.js
+++ posts/post-router.js
@@ -11,19 +11,19 @@ router.get("/", (req, res) => {
       res.json(posts);
     })
     .catch((err) => {
-      res.status(500).json({ message: "error getting posts", error: err });
+      res.status(500).json({ message: "Error getting posts!", error: err });
     });
 });
 
 router.get("/:id", await, (req, res) => {
-//   db("posts")
-//     .where("/:id", req.params.id)
-//     .then((posts) => {
-//       res.json(posts);
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ message: "invalid ID", error: err });
-//     });
+  db("posts")
+    .where("/:id", req.params.id)
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "invalid ID", error: err });
+    });
     const { id } = req.params;
 
     try {
@@ -32,10 +32,10 @@ router.get("/:id", await, (req, res) => {
         if (post) {
             res.json(post);
         } else {
-            res.status(404).json({ message: 'Bad ID'})
+            res.status(404).json({ message: 'Bad ID!' });
         }
     } catch (err) {
-        res.status(500).json({ message: 'db error', error : err })
+        res.status(500).json({ message: 'db error!', error: err });
     }
 });
 
@@ -48,12 +48,41 @@ router.post("/", async, (req, res) => {
         res.status(201).json(post);
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: 'error posting', error : err})
+        res.status(500).json({ message: 'Error posting!', error: err });
     }
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", async, (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
 
-router.delete("/:id", (req, res) => {});
+    try {
+        const count = await db('posts').update(changes).where({ id });
+        
+        if (count) {
+            res.json({ updated: count });
+        } else {
+            res.status(404).json({ message: "Invalid ID!" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Error updating record!", error: err });
+    }
+});
+
+router.delete("/:id", async, (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const count = await db('posts').where({ id }).del();
+
+        if (count) {
+            res.json({deleted: count})
+        } else {
+            res.status(404).json({ message: "Invalid ID!", error: err });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Error deleting record!", error: err });
+    }
+});
 
 module.exports = router;
