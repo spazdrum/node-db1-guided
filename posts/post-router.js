d5b567aa020de5e02f21ba91063cc0f28816259d
spazdrum <spazdrum@gmail.com>
2020-09-15 23:07:40 -0400

    get('/') and get('/:id')

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  db("posts")
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "error getting posts", error: err });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  db("posts")
+    .where("/:id", req.params.id)
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "invalid ID", error: err });
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.put("/:id", (req, res) => {});
 
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
